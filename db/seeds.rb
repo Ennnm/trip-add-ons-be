998b099b3c6320b0cc8c29e296ff85eac503c9ec
en <jiaen.1sc4@gmail.com>
2024-05-05 11:30:58 +0200

    fix: add cost to add_ons, fix startTime endTime data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,34 +12,39 @@ Trip.destroy_all
 AddOn.destroy_all
 TripAddOn.destroy_all
 
-Trip.create!([{
+trip1 = Trip.create!([{
   name: "Prague 7d6n",
 }])
 
-AddOn.create!([{
+addOns = AddOn.create!([{
   name: "Single room upgrade",
   total_spots: 100,
+  cost: 49,
 }, {
   name: "Single room extreme upgrade",
   total_spots: 1,
+  cost: 100
 }, {
   name: "Visit to cultural house",
-  start_time: DateTime.new(2024, 6, 1, 15).to_time.to_i,
-  end_time: DateTime.new(2024, 6, 1, 17).to_time.to_i,
+  start_time: Time.new(2024, 6, 1, 15),
+  end_time: Time.new(2024, 6, 1, 17),
   total_spots: 100,
+  cost: 20
 }, {
   name: "Visit to theatre",
-  start_time: DateTime.new(2024, 6, 1, 14).to_time.to_i, # conflicting time entry
-  end_time: DateTime.new(2024, 6, 1, 16).to_time.to_i,
+  start_time: Time.new(2024, 6, 1, 14), # conflicting time entry
+  end_time: Time.new(2024, 6, 1, 16),
   total_spots: 100,
+  cost: 25
 }, {
   name: "Visit to beer garden",
-  start_time: DateTime.new(2024, 6, 1, 18).to_time.to_i,
-  end_time: DateTime.new(2024, 6, 1, 20).to_time.to_i,
+  start_time: Time.new(2024, 6, 1, 18),
+  end_time: Time.new(2024, 6, 1, 20),
   total_spots: 100,
+  cost: 30
 }])
 
 TripAddOn.create!([{
-  trip_id: 1,
-  add_on_id: 2,
+  trip_id: trip1[0].id,
+  add_on_id: addOns[1].id,
 }])
