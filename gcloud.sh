63c8954b225e41a5330906be62b155b3840f4a3e
en <jiaen.1sc4@gmail.com>
2024-05-05 11:32:01 +0200

    fix: gcloud run script

--- gcloud.sh
+++ gcloud.sh
@@ -11,6 +11,6 @@ export IMAGE="tripaddons"
 export IMAGE_TAG=${REGION}-docker.pkg.dev/$GCLOUD_PROJECT/$REPO/$IMAGE
 
 # Build the image:
-# docker build -t $IMAGE_TAG -f Dockerfile --platform linux/x86_64 .
+docker build -t $IMAGE_TAG -f Dockerfile --platform linux/x86_64 .
 # Push it to Artifact Registry:
 docker push $IMAGE_TAG
